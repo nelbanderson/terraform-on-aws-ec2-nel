ad58416944a4f4832a899b8d1951c02df992fd30
nelbanderson <nelbanderson@gmail.com>
2022-12-05 11:33:01 -0500

    change IAM in region

--- 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
+++ 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
@@ -16,6 +16,6 @@ provider "aws" {
 
 # Resource Block
 resource "aws_instance" "ec2demo" {
-  ami           = "ami-0533f2ba8a1995cf9" # Amazon Linux in us-east-1, update as per your region
+  ami           = "ami-0b0dcb5067f052a63" # Amazon Linux in us-east-1, update as per your region
   instance_type = "t2.micro"
 }
